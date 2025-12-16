4efc1dbb6ce08f9d6d2491d02fff43ae61cb0fda
boris <boradmy@gmail.com>
2025-12-15 23:13:30 -0500

    FIN PRACTICA

--- lab8/urls.py
+++ lab8/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('o/', include(oauth2_urls)),
+    path('api/', include('api.urls')),
 
 ]
 
